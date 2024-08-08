229e06a6de530540c651191c4e94340a6d94fdaa
SRINJOY DAS <115316528+srinju@users.noreply.github.com>
2024-08-08 21:26:11 +0530

    Create TwoButtons.jsx

---
+++ src/components/TwoButtons.jsx
@@ -0,0 +1,12 @@
+
+
+export const TwoButtons = () => {
+    return <div className="flex justify-start p-4">
+        <div>
+            <button type="button" class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Cyan to Blue</button>
+        </div>
+        <div>
+            <button type="button" class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Cyan to Blue</button>
+        </div>
+    </div>
+}
